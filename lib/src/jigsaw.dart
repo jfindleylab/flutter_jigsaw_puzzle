c9d136dcf10713676600ebf8b059c0a4007571b4
jfindleylab <jerryfndly@gmail.com>
2024-08-17 22:47:23 -0500

    update code after flutter and dart update

--- lib/src/jigsaw.dart
+++ lib/src/jigsaw.dart
@@ -116,7 +116,7 @@ class JigsawWidgetState extends State<JigsawWidget> {
     if (pngBytes == null) {
       throw InvalidImageException();
     }
-    return ui.decodeImage(List<int>.from(pngBytes));
+    return ui.decodeImage(Uint8List.fromList(pngBytes));
   }
 
   void reset() {
@@ -181,10 +181,10 @@ class JigsawWidgetState extends State<JigsawWidget> {
 
         final ui.Image temp = ui.copyCrop(
           fullImage!,
-          xAxis.round(),
-          yAxis.round(),
-          widthPerBlockTemp.round(),
-          heightPerBlockTemp.round(),
+          x: xAxis.round(),
+          y: yAxis.round(),
+          width: widthPerBlockTemp.round(),
+          height: heightPerBlockTemp.round(),
         );
 
         final Offset offset = Offset(size!.width / 2 - widthPerBlockTemp / 2,
